b83e95ce0b8e105f0f76654bbb7b7fea14398fc4
hmtmcse <hmtm.cse@gmail.com>
2022-03-17 16:53:06 +0600

    update auth system

--- zsamples/xyz-view.tsx
+++ zsamples/xyz-view.tsx
@@ -1,6 +1,7 @@
 import {PFProps} from "@pfo/pf-react/src/artifacts/interface/pf-mixed-interface";
 import PFComponentState from "@pfo/pf-react/src/artifacts/component/pf-component-state";
 import PFComponent from "@pfo/pf-react/src/artifacts/component/pf-component";
+import {PFAppContext} from "@pfo/pf-react/src/artifacts/config/pf-app-context";
 
 interface Props extends PFProps {}
 
@@ -10,6 +11,7 @@ class State extends PFComponentState {}
 export default class XyzView extends PFComponent<Props, State> {
 
     state: State = new State();
+    static contextType = PFAppContext
 
     static defaultProps = {}
 
