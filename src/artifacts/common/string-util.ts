41ab779df480db4100b5f45725640f13edbc9f88
hmtmcse <hmtm.cse@gmail.com>
2022-05-29 12:25:21 +0600

    added new string util

---
+++ src/artifacts/common/string-util.ts
@@ -0,0 +1,24 @@
+export default class StringUtil {
+
+    public static capitalize(word: string) {
+        return word.charAt(0).toUpperCase() + word.substring(1);
+    }
+
+    public static camelCaseToHumanReadable(text: string) {
+        let words = text.match(/[A-Za-z][a-z]*/g) || [];
+        return words.map(this.capitalize).join(" ");
+    }
+
+    public static smallFirstLatter(word: string) {
+        return word.charAt(0).toLowerCase() + word.slice(1);
+    }
+
+    public static capitalizeFirstLetter(word: string) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
+    public static camelCaseTo(text: string, char: string = "_") {
+        return text.replace(/[A-Z]/g, letter => char + letter);
+    }
+
+}
